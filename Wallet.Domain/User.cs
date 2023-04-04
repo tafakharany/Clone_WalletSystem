2ec54e669c97fd916c9e0228f6c1a657d6fd35c4
tafakharany <taha.ashri96@outlook.com>
2023-04-04 20:29:28 +0200

    initialize Wallet System

---
+++ Wallet.Domain/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wallet.Domain
+{
+    [Table(name:"User")]
+    public class User
+    {
+        public int Id { get; set; }
+        public string MobileNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
