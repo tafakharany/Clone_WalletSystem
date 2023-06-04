321fedf53f34bceb77335fbbbf2c4afb22ed69e7
Taha Elfakharany <31326367+tafakharany@users.noreply.github.com>
2023-06-05 02:24:10 +0300

    Add views for repoerts of transactions

---
+++ Wallet.Domain/Views/TransactionsGiftedFromAdmin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Wallet.Domain.Views;
+
+public partial class TransactionsGiftedFromAdmin
+{
+    public int? Id { get; set; }
+
+    public int? RecipientId { get; set; }
+
+    public decimal? Amount { get; set; }
+
+    public DateTime? Date { get; set; }
+
+    public string? Name { get; set; }
+}
